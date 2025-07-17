acd0578a5d0c9df45faa1bab3cf626e4b6f1d800
Orhan Ekici <orhanekici@gmail.com>
2025-07-17 12:21:04 +0300

    viewdata tempdata partial section örnekleri

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,14 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        // controller üzerinden view'a veri göndermek için kullanırız
+        // ViewBag dynamic obje gibi çalışır. okunurluğu iyidir ama ViewData'ya göre yavaştır
+        // ViewData dictionary gibi çalışır. hızlıdır.
+        // TempData dictionary gibi çalışır ama 1 kere çalışır
+        // ViewBag.Name = "Orhan";
+        ViewData["Name"] = "Orhan";
+        ViewData["BirthDate"] = 2000;
+        
         return View();
     }
 
@@ -13,4 +21,12 @@ public class HomeController : Controller
     {
         return View();
     }
+
+    public IActionResult Add()
+    {
+        // belirli kontroller yaptım varsayalım
+        TempData["ErrorMessage"] = "Bunu ekleyemem üzgünüm...";
+        return RedirectToAction("Index");
+    }
+    
 }
